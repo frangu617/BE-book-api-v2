a20a5c165e9aad87f37f9914239fdc33a2016a7c
frangu617 <142462584+frangu617@users.noreply.github.com>
2023-12-08 11:21:08 -0800

    done

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // DEPENDENCIES
 const express = require('express')
+const cors = require('cors')
 const mongoose = require('mongoose')
 
 
@@ -12,15 +13,18 @@ mongoose.connect(process.env.MONGO_URI)
     .catch(err => console.error('Could not connect to MongoDB:', err));
 
 // MIDDLEWARE
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 
 
 // ROUTES
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
   res.send('Welcome to the Book API')
 })
+app.get('/books/:_id', (req, res, next) => {
+    res.json({ message: 'This is the book route' })}    )
 
 // Book: 
 const bookController = require('./controllers/books_controller.js')
